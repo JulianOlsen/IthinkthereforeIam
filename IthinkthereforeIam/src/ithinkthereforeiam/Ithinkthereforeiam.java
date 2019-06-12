d9d07df49f985a04a2eb3f262980b3fe8c132da1
Julian Olsen <46683927+JulianOlsen@users.noreply.github.com>
2019-06-12 18:03:00 +0200

    Add files via upload

---
+++ IthinkthereforeIam/src/ithinkthereforeiam/Ithinkthereforeiam.java
@@ -0,0 +1,19 @@
+package ithinkthereforeiam;
+import java.util.Scanner;
+ 
+public class Ithinkthereforeiam {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner (System.in);
+        Scanner scane = new Scanner (System.in);
+        String think;
+        String am;
+        System.out.println("First word");
+        think = scan.nextLine();
+        System.out.println("second word");
+        am = scane.nextLine();
+        System.out.println("I "+think+", therefore I "+am);
+        
+
+    }   
+}
